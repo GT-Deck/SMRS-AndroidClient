29a5f30c5d6588b01897c7ce58d04ebbe0f08ffa
GT-Deck <deckersjd@yahoo.com>
2014-11-09 21:03:52 -0800

    UdpTransaction now implements Runnable
    UdpTransaction class now implements Runnable, no need to return Future
    object. Modified CustomArrayAdapter to accommodate changes to
    UdpTransaction, and began work on SMRSMain

--- SMRSRemote/src/com/decker/smrsremote/SMRSMain.java
+++ SMRSRemote/src/com/decker/smrsremote/SMRSMain.java
@@ -1,4 +1,7 @@
 package com.decker.smrsremote;
+
+import java.util.ArrayList;
+
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,48 +11,51 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 
-public class SMRSMain extends ListActivity{
-	
-	ListView usersServerList; 
-	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) 
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_smrsmain);
-        usersServerList = getListView();
-        
-        usersServerList.setOnItemClickListener(new OnItemClickListener()
-		{
+public class SMRSMain extends ListActivity {
+
+	ListView usersServerList;
+	ArrayList<StarMadeServer> servers;
+	DBCommander DBManager;
+	int ListSelection;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_smrsmain);
+		usersServerList = getListView();
+
+		usersServerList.setOnItemClickListener(new OnItemClickListener() {
 			@Override
 			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
 					long arg3) {
-				// TODO  Selection of which servers to remove will be done here
-				
+				arg1.setSelected(true);
+				ListSelection = arg2;
+
 			}
 		});
-    }
+	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// TODO Inflate the menu; this adds items to the action bar if it is
+		// present.
+		getMenuInflater().inflate(R.menu.smrsmain, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // TODO Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.smrsmain, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-    @Override
-	public boolean onOptionsItemSelected(MenuItem item){
-		switch(item.getItemId())
-		{
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
 		case R.id.delete_button:
-			//TODO removal of selected server is done here
-				return true;
-		case R.id.add_button:	
-			//TODO addition of new server is done here
-				return true;
+
+			// TODO removal of selected server is done here
+			return true;
+		case R.id.add_button:
+			// TODO addition of new server is done here
+			return true;
 		default:
 			return super.onOptionsItemSelected(item);
-		}	
+		}
 	}
-    
+
 }
